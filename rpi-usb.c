ca343e9c73a0606aadb10ff9a668cdf45c72a205
5StringsOfDoom <thestompbox1983@gmail.com>
2018-04-03 20:21:41 +0100

    Update rpi-usb.c

--- rpi-usb.c
+++ rpi-usb.c
@@ -2,15 +2,6 @@
 
 // initialize USB
 void usb_init(void) {
-	HW_CONFIG();
-	USB_FREEZE();				// enable USB
-	PLL_CONFIG();				// config PLL
-	while (!(PLLCSR & (1<<PLOCK)));		// wait for PLL lock
-	USB_CONFIG();				// start USB clock
-	UDCON = 0;				// enable attach resistor
-	usb_configuration = 0;
-	UDIEN = (1<<EORSTE)|(1<<SOFE);
-	sei();
 }
 
 // return 0 if the USB is not configured, or the configuration
